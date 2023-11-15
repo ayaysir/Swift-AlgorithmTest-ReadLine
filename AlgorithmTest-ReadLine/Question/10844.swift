741bbff4fa9cd6ccf7adc5eda76bd4fb23a56b98
윤범태 <yoonbumtae@gmail.com>
2023-11-16 00:16:46 +0900

    Added 10844 (쉬운 계단 수)

---
+++ AlgorithmTest-ReadLine/Question/10844.swift
@@ -0,0 +1,8 @@
+//
+//  10844.swift
+//  AlgorithmTest-ReadLine
+//
+//  Created by 윤범태 on 2023/11/15.
+//
+
+import Foundation
---
+++ AlgorithmTest-ReadLine/Question/15486.swift
@@ -0,0 +1,8 @@
+//
+//  Q_15486.swift
+//  AlgorithmTest-ReadLine
+//
+//  Created by 윤범태 on 2023/11/16.
+//
+
+import Foundation
